d1f2f4984900076fe6ab5273864d6112af2620a9
Vitalii Elenhaupt <velenhaupt@gmail.com>
2020-10-07 20:29:20 +0300

    Add ability to validate .tracker.yml

--- lib/traker/instrumenter.rb
+++ lib/traker/instrumenter.rb
@@ -1,25 +1,24 @@
 # frozen_string_literal: true
 
-require 'yaml'
-
 module Traker
   # Wraps array of rake tasks, and auguments each one of them
   # with Traker features
   class Instrumenter
-    attr_accessor :tasks
+    attr_accessor :tasks, :config
 
     def initialize(tasks)
       @tasks = tasks
+      @config = Traker::Config.load
     end
 
     def instrument
       tasks.each do |t|
         task_name = t.name
 
-        next unless tasks_to_be_run.map { |task| task['name'] }.include?(task_name)
+        next unless config.tasks_to_be_run.map { |task| task['name'] }.include?(task_name)
 
         handler = proc do |&block|
-          record = Traker::Task.find_or_initialize_by(name: task_name, environment: env)
+          record = Traker::Task.find_or_initialize_by(name: task_name, environment: config.env)
 
           record.started_at = DateTime.now
           record.is_success = true
@@ -39,20 +38,6 @@ module Traker
 
         yield task_name, handler
       end
-    end
-
-    private
-
-    def config
-      @config ||= YAML.safe_load(File.read(File.join(Rails.root, '.traker.yml')))
-    end
-
-    def env
-      @env ||= ENV.fetch('TRAKER_ENV', 'default')
-    end
-
-    def tasks_to_be_run
-      config['environments'][env] || []
     end
   end
 end
